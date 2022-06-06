d6e536f34df6658225b619690a0b75cad1e5f1ef
Edarcega <edimar.eds@gmail.com>
2022-06-06 19:49:11 -0300

    Projeto criado

---
+++ src/test/java/com/edstecno/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edstecno.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
