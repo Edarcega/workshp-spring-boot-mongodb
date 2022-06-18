93d649be59019034ace0949ca7b865c1a8f9dd0c
Edarcega <edimar.eds@gmail.com>
2022-06-18 14:16:19 -0300

    Consulta simples com query methods

--- src/main/java/com/edstecno/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/edstecno/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package com.edstecno.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.edstecno.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
-	
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	List<Post> findByTitleContainingIgnoreCase(String txt);
+
 }
